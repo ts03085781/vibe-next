3fdebfe2c1affd3784d910ac8e6e683c0b0e7ee7
Dean Lai <ts03085781@gmail.com>
2025-07-31 21:56:41 +0800

    fix: add missing form data of rating

--- src/app/api/mangas/route.ts
+++ src/app/api/mangas/route.ts
@@ -141,6 +141,7 @@ export async function POST(request: NextRequest) {
     const now = new Date();
     const mangaData = {
       ...body,
+      rating: 0,
       year: now.getFullYear(),
       totalChapters: 0,
       createDate: now,
